08c79a942f9beb6ea1db048a9e857a2d84361d07
HodayaGitHub <hodaya1812@gmail.com>
2024-04-24 20:11:28 +0300

    Feature: Add navigation link to the main page in the header navbar

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -6,7 +6,9 @@ export function AppHeader() {
     return (
         <header className="app-header full main-layout">
             <section className="header-container">
-                <h1>Ad Campaign Manager</h1>
+                <NavLink to="/campaign" className="nav-link">
+                    <h1>Ad Campaign Manager</h1>
+                </NavLink>
                 <nav className="app-nav">
                     <NavLink className="header-link" to="/campaign">Campaigns</NavLink>
                     <NavLink className="header-link" to="/dashboard">Dashboard</NavLink>
