7a6fd70e045e1c45b81db9b504cd39e626a43626
HodayaGitHub <hodaya1812@gmail.com>
2024-04-25 10:04:36 +0300

    Enhancement: Update font family and refine table design

--- src/cmps/CampaignList.jsx
+++ src/cmps/CampaignList.jsx
@@ -1,28 +1,29 @@
 import { CampaignPreview } from "./CampaignPreview.jsx"
 
-export function CampaignList({ campaigns, onRemoveCampaign, onEditCampaign, addToCampaign, onCampaignDetails}) {
+export function CampaignList({ campaigns, onRemoveCampaign, onEditCampaign, addToCampaign, onCampaignDetails }) {
 
     return (
         <table className="campaign-table">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Advertising Platform</th>
-                <th>Advertiser Landing Page</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            {campaigns.map(campaign =>
-                <CampaignPreview
-                    key={campaign._id}
-                    campaign={campaign}
-                    onRemoveCampaign={onRemoveCampaign}
-                    onCampaignDetails={onCampaignDetails}
-                    onEditCampaign={onEditCampaign}
-                />
-            )}
-        </tbody>
-    </table>
+            <thead>
+                <tr>
+                    <th>Banner image </th>
+                    <th>Name</th>
+                    <th>Advertising Platform</th>
+                    <th>Advertiser Landing Page</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {campaigns.map(campaign =>
+                    <CampaignPreview
+                        key={campaign._id}
+                        campaign={campaign}
+                        onRemoveCampaign={onRemoveCampaign}
+                        onCampaignDetails={onCampaignDetails}
+                        onEditCampaign={onEditCampaign}
+                    />
+                )}
+            </tbody>
+        </table>
     )
 }
