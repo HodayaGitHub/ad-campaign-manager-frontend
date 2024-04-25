bddbf6cfb193e9fe6346b63e19d87dd0b7f84a54
HodayaGitHub <hodaya1812@gmail.com>
2024-04-25 11:34:52 +0300

    Improve layout and design

--- src/cmps/statistics/PlatformStatistics.jsx
+++ src/cmps/statistics/PlatformStatistics.jsx
@@ -3,11 +3,15 @@ import { BarChart } from '@mui/x-charts/BarChart';
 
 export function PlatformStatistics() {
     return (
-        <BarChart
-            xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
-            series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
-            width={500}
-            height={300}
-        />
+
+        <div className='statistics-item'>
+            <BarChart
+                xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
+                series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+                width={400}
+                height={300}
+            />
+        </div>
+
     );
 }
